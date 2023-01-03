6b16e40f49ce1b776b22211bf4e97393c722fbc2
emanuel-gauna <lemagauna@gmail.com>
2023-01-02 22:28:20 -0300

    Crear la estructura de archivos utilizando Node.js+Express

---
+++ app/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+
+app.use(express.static("public"));
+
+
+/* home */
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/index.html"))
+});
+/* detalle de producto */
+app.get("/producDetail", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/productDetail.html"))
+});
+/* carrito de compras */
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/productCart.html"))
+});
+/* formulario de registro*/
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+/* login */
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+});
+
+app.listen( PORT, ()=>console.log(`server listen in port ${PORT}\n http://localhost:${PORT}` ))
