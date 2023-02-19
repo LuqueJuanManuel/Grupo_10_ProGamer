101b0dcd10bf819133612a97c445b73ed0435bdd
JuanLuque <juanluquejml@gmail.com>
2023-02-19 17:57:51 -0300

    Se agrega la vista Search, ruta y cointroladores se aplican cambios en el header

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -3,10 +3,19 @@ const { readJSON, writeJSON } = require('../dataBase/');
 
 const products = readJSON('products.json');
 
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
 module.exports = {
   index: (req, res) => {
+    const productosEnOferta = products.filter(product => product.discount >= 20);
+    const productosDestacados = products.filter(product => product.price <= 50000);
+    
+
     res.render("index", {
       products, 
+      productosEnOferta,
+      productosDestacados,
+      toThousand,
     })
   }
 }
