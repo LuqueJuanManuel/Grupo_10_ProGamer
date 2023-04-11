45fd5b1dc7e3b4ce3eab79f723bec16895b451c4
JuanLuque <juanluquejml@gmail.com>
2023-04-11 16:29:57 -0300

    modificaciones e  indexController

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -1,12 +1,15 @@
 /* Requires */
-const { readJSON, writeJSON } = require('../dataBase/');
-
-const products = readJSON('products.json');
-
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const { Image, Product_category, Product, User_category, User, Sequelize } = require('../database/models');
+const { Op } = Sequelize;
 
 module.exports = {
   index: (req, res) => {
+    Product.findAll({
+      include: [{association: 'product_category'}, {association: 'images'}]
+  })
+  .then(products => {
+    res.render('index.ejs', { products})
+  })
     const productosEnOferta = products.filter(product => product.discount >= 20);
     const productosDestacados = products.filter(product => product.price <= 50000);
     
