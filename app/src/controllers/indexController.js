6e531c0548e12ddf3902b57fe781ef703a8ae3a0
JuanLuque <juanluquejml@gmail.com>
2023-04-11 17:02:14 -0300

    MERGE CONFLICT

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -1,12 +1,5 @@
 /* Requires */
-<<<<<<< HEAD
-const {} = require('../database/models');
-=======
-/* const { readJSON, writeJSON } = require('../oldDatabase/');
-const products = readJSON('products.json');
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."); */
 const {Product, Banner,sequelize} = require('../database/models');
->>>>>>> 81d56c93ae1af388bf66e1818654bfae3dc5d8e9
 
 
 module.exports = {
@@ -23,17 +16,6 @@ module.exports = {
       console.log(products)
       return res.send(products)
     })
-    /* const productosEnOferta = products.filter(product => product.discount >= 20);
-    const productosDestacados = products.filter(product => product.price <= 50000);
-    
-
-    res.render("index", {
-      products, 
-      productosEnOferta,
-      productosDestacados,
-      session: req.session,
-      toThousand,
-    }) */
   }
 }
 
