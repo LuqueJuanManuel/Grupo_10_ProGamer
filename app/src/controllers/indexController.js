fd80d80d8199cfb5a310b133e12c144b09d4c5ff
JuanLuque <juanluquejml@gmail.com>
2023-04-17 19:45:08 -0300

    index controller

--- app/src/controllers/indexController.js
+++ app/src/controllers/indexController.js
@@ -1,10 +1,23 @@
 /* Requires */
-const {Product, Banner,sequelize} = require('../database/models');
-
+const { Product, Banner, sequelize } = require('../database/models');
 
 module.exports = {
   index: (req, res) => {
-    Banner.findAll()
+    const banner = Banner.findAll();
+    
+    const productosDestacados = Product.findAll({
+       where: {
+        price: { [Op.lte]: 50000 }
+       }
+    });
+
+    const productosEnOferta = Product.findAll({
+      where: {
+        discount: { [Op.gte]: 20 }
+      }
+    }); 
+    
+    /* Banner.findAll()
     Product.findAll({
       include:[{association: 'images'}]
     },{
@@ -15,7 +28,7 @@ module.exports = {
     .then(products =>{
       console.log(products)
       return res.send(products)
-    })
+    }) */
   }
 }
 
