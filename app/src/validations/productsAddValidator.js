f54880968aa440f4eea4a87d1de2c72fb020e3ca
JuanLuque <juanluquejml@gmail.com>
2023-05-07 17:02:40 -0300

    Se sube las validaciondes de agregar un producto

--- app/src/validations/productsAddValidator.js
+++ app/src/validations/productsAddValidator.js
@@ -1,23 +1,23 @@
 const { check } = require("express-validator");
 
 module.exports = [
-    check("name")
+    check("name") 
     .notEmpty().withMessage("El nombre es obligatorio").bail()
     .isLength({ min: 3, max: 20 }).withMessage("El nombre debe tener entre 3 y 20 caracteres"),
 
-check("category")
+check("category") 
     .notEmpty().withMessage("Debes indicar la categoría"),
 
-check("brand")
+check("brand") 
     .notEmpty().withMessage("Debes indicar la marca"),
 
-check("description")
+check("description") 
     .notEmpty().withMessage("La descripción es obligatoria").bail()
     .isLength({ min: 5}).withMessage("La descripción debe tener mínimo 5 caracteres"),
 
-check("price")
+check("price") 
     .isInt({min:1}).withMessage("Debes indicar un precio"),
 
-check("discount")
+check("discount") 
     .isInt({min:0,max:99}).withMessage("El descuento no puede ser más del 100%")
 ]
