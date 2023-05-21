6ae09809931d5a4c21fa1711c86503f696f8d27c
emanuel-gauna <lemagauna@gmail.com>
2023-05-21 15:19:30 -0300

    agregando validacion de tipo de archivos permitidos (jpg,web,jpeg,png,gif) en subida imagen de registro de nuevo usuario

--- app/public/JS/validations/userRegisterValidator.js
+++ app/public/JS/validations/userRegisterValidator.js
@@ -20,7 +20,9 @@ w.addEventListener("load", () => {
         $pass2 = qs("#pass2"),
         $pass2Errors = qs("#pass2Errors"),
         $form = qs("#form"),
-        $submitErrors = qs("#submitErrors")
+        $submitErrors = qs("#submitErrors"),
+        $file = qs("#image"),
+        $fileErrors = qs("#fileErrors")
         regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/,//dato alfabetico
         regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,//dato mail
         regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;//dato contraseña
@@ -149,5 +151,27 @@ w.addEventListener("load", () => {
             }
          })
     
+         $file.addEventListener('change', () => {
+            let filePath = $file.value, //Capturo el valor del input
+                allowefExtensions = /(.jpg|.jpeg|.png|.gif|.web)$/i //Extensiones permitidas
+            if(!allowefExtensions.exec(filePath)){ //El método exec() ejecuta una busqueda sobre las coincidencias de una expresión regular en una cadena especifica. Devuelve el resultado como array, o null.
+                $fileErrors.innerHTML = 'Carga un archivo de imagen válido, con las extensiones (.jpg - .jpeg - .png - .gif)';
+                $file.value = '';
+                $imgPreview.innerHTML = '';
+                return false;
+            }else{
+                // Image preview
+                console.log($file.files);
+                if($file.files && $file.files[0]){
+                    let reader = new FileReader();
+                    reader.onload = function(e){
+                        $imgPreview.innerHTML = '<img src="' + e.target.result +'"/>';
+                    };
+                    reader.readAsDataURL($file.files[0]);
+                    $fileErrors.innerHTML = '';
+                    $file.classList.remove('is-invalid')
+                }
+            }
+        })
 
 })
